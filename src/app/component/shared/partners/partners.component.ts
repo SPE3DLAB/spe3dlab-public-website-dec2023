512082b8f58d69e72bcfad135835eb25c3f9ed31
AnhNguyenTung039 <nguyentunganh039@gmail.com>
2024-01-09 12:05:41 +0700

    Removes 2 partners at the end of partners component

--- src/app/component/shared/partners/partners.component.ts
+++ src/app/component/shared/partners/partners.component.ts
@@ -16,39 +16,27 @@ export class PartnersComponent {
   partners: PartnerInterface[] = [
     {
       routerLink: 'partners',
-      class: 'col-2 p-3',
+      class: 'col p-3',
       imageSrc: '/assets/images/partners/partner-latim-logo.gif',
       imageName: 'latim-logo',
     },
     {
       routerLink: 'partners',
-      class: 'col-2',
+      class: 'col',
       imageSrc: '/assets/images/partners/partner-chu-logo.jpeg',
       imageName: 'chu-logo',
     },
     {
       routerLink: 'partners',
-      class: 'col-2 p-3',
+      class: 'col p-3',
       imageSrc: '/assets/images/partners/partner-iris-logo.png',
       imageName: 'iris-logo',
     },
     {
       routerLink: 'partners',
-      class: 'col-2',
+      class: 'col',
       imageSrc: '/assets/images/partners/partner-saint-denis-logo.png',
       imageName: 'saint-denis-logo',
-    },
-    {
-      routerLink: 'partners',
-      class: 'col-2',
-      imageSrc: '/assets/images/partners/partner-french-tech-logo.jpg',
-      imageName: 'french-tech-logo',
-    },
-    {
-      routerLink: 'partners',
-      class: 'col-2',
-      imageSrc: '/assets/images/partners/partner-systematic-logo.png',
-      imageName: 'systematic-logo',
     },
   ];
 }
